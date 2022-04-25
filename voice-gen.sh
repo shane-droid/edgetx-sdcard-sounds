a86376bb655caa5ef061e3de3a83fa6290cc72a3
Peter Feerick <peter.feerick@gmail.com>
2022-04-25 21:40:39 +1000

    feat: Add line number count to generation script

--- voice-gen.sh
+++ voice-gen.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 generate_lang () {
+  # $1: CSV file
+  # $2: voice name
+  # $3: language directory
+
+  ROOT=SOUNDS
+  DELAY=3
+  CSV_FILE=$1
+  VOICE_NAME=$2
+  LANG=$3
+
+  LINE_NUMBER=0
+  NUM_OF_LINES=$(wc -l < "$CSV_FILE")
+  NUM_OF_LINES=$((NUM_OF_LINES-1))
+
+  echo "Generating $VOICE_NAME ($LANG)... $NUM_OF_LINES lines"
+
   while read -r line
   do
     # consume header line
@@ -9,10 +25,9 @@ generate_lang () {
         continue
     fi
 
-    ROOT=SOUND
-    DELAY=3
+    LINE_NUMBER=$((LINE_NUMBER+1))
+
     SUBDIR=$(echo -n "$line" | awk -F ';' '{print $1}')
-    LANG=$3
     FILENAME=$(echo -n "$line" | awk -F ';' '{print $2}')
     TEXT=$(echo -n "$line" | awk -F ';' '{print $3}')
 
@@ -25,11 +40,11 @@ generate_lang () {
     [ ! -d "$OUTDIR" ]  && mkdir -p "$OUTDIR"
 
     if test -f "$OUTDIR/$FILENAME"; then
-      echo "File '$OUTDIR/$FILENAME' already exists. Skipping."
+      echo "($LINE_NUMBER/$NUM_OF_LINES) File '$OUTDIR/$FILENAME' already exists. Skipping."
     else
-      echo "File '$OUTDIR/$FILENAME' does not exist. Creating..."
-      spx synthesize --text \""$TEXT"\" --voice "$2" --audio output "$OUTDIR/$FILENAME" || break
+      echo "($LINE_NUMBER/$NUM_OF_LINES) File '$OUTDIR/$FILENAME' does not exist. Creating... "
+      spx synthesize --text \""$TEXT"\" --voice "$VOICE_NAME" --audio output "$OUTDIR/$FILENAME" || break
       sleep $DELAY
     fi
-  done < "$1"
+  done < "$CSV_FILE"
 }
